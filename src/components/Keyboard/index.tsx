c496d86d401e00fcd4e318d148d8c6a035b22c67
Artem K <pagesppl@gmail.com>
2023-02-05 03:32:48 +0700

    fix bugs

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -8,44 +8,46 @@ import Hands from '../Hands';
 
 import styles from './index.module.css';
 
-interface Keyboard {
+interface KeyboardProps {
   sidebarOpen: boolean;
 }
 
 type CheckTextPick = Pick<CheckText, 'writtenText' | 'wordVariants' | 'currentVariantIndex'>;
 
-const Keyboard: FC<Keyboard & CheckTextPick> = memo(({writtenText, wordVariants, currentVariantIndex, sidebarOpen}) => {
-  const [nextTypeKey, setNextTypeKey] = useState<NextTypeKey | null>(null);
-  const currentLayout: keyboardLayout = keyboardLayout.MAC;
-  const keyboard = useMemo(() => keyboards.find((keyboard) => keyboard.layoutKey === currentLayout), [currentLayout]);
-  const currentWordVariant = useMemo(
-    () => wordVariants.find((item, index) => index === currentVariantIndex),
-    [currentVariantIndex, wordVariants]
-  );
-  const nextKey = currentWordVariant?.variant[writtenText.length];
+const Keyboard: FC<KeyboardProps & CheckTextPick> = memo(
+  ({writtenText, wordVariants, currentVariantIndex, sidebarOpen}) => {
+    const [nextTypeKey, setNextTypeKey] = useState<NextTypeKey | null>(null);
+    const currentLayout: keyboardLayout = keyboardLayout.MAC;
+    const keyboard = useMemo(() => keyboards.find((keyboard) => keyboard.layoutKey === currentLayout), [currentLayout]);
+    const currentWordVariant = useMemo(
+      () => wordVariants.find((item, index) => index === currentVariantIndex),
+      [currentVariantIndex, wordVariants]
+    );
+    const nextKey = currentWordVariant?.variant[writtenText.length];
 
-  const onChangeNextTypeKey = useCallback((obj: NextTypeKey) => setNextTypeKey(obj), []);
+    const onChangeNextTypeKey = useCallback((obj: NextTypeKey) => setNextTypeKey(obj), []);
 
-  return (
-    <div className={styles.keyboard}>
-      {nextTypeKey ? <Hands nextTypeKey={nextTypeKey} /> : null}
-      <div>
-        {keyboard?.keys.map((line, index) => (
-          <div key={index} className={styles.keyLine}>
-            {line.map((key, index) => (
-              <KeyboardKey
-                key={index}
-                keyboardKey={key}
-                nextKey={nextKey}
-                onChangeNextTypeKey={onChangeNextTypeKey}
-                sidebarOpen={sidebarOpen}
-              />
-            ))}
-          </div>
-        ))}
+    return (
+      <div className={styles.keyboard}>
+        {nextTypeKey ? <Hands nextTypeKey={nextTypeKey} /> : null}
+        <div>
+          {keyboard?.keys.map((line, index) => (
+            <div key={index} className={styles.keyLine}>
+              {line.map((key, index) => (
+                <KeyboardKey
+                  key={index}
+                  keyboardKey={key}
+                  nextKey={nextKey}
+                  onChangeNextTypeKey={onChangeNextTypeKey}
+                  sidebarOpen={sidebarOpen}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Keyboard;
--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -24,6 +24,7 @@ export function useKeyPress(targetKey: string) {
       window.removeEventListener('keydown', downHandler);
       window.removeEventListener('keyup', upHandler);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Empty array ensures that effect is only run on mount and unmount
   return keyPressed;
 }
