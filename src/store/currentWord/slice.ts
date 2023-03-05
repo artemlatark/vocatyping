950a0e59cf4a281c49fc5cacb02561eb5953cb59
Artem K <pagesppl@gmail.com>
2023-03-05 17:21:19 +0700

    small fixes (#29)

--- src/store/currentWord/slice.ts
+++ src/store/currentWord/slice.ts
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction, current} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {getFromLocalStorage, saveToLocalStorage} from 'utils';
 import {State, ChangeWordPayloadAction} from './types';
 
