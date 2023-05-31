420f07bfe88def9c7e4a11bd6815c44a179f4f25
Artem K <pagesppl@gmail.com>
2023-06-01 01:35:32 +0700

    #66 added new test for words actionCreators

--- src/store/words/actionCreators.ts
+++ src/store/words/actionCreators.ts
@@ -12,34 +12,38 @@ import {Word} from 'models/Word';
 
 interface FetchDictionaryArgs {
   initialWordId: number;
+  dictionaryURL?: string;
 }
 
-export const fetchDictionary = createAsyncThunk('words/fetchDictionary', async ({initialWordId}: FetchDictionaryArgs, {dispatch, rejectWithValue}) => {
-  try {
-    const dictionaryRef = ref(firebaseStorage, 'dictionaries/shestov.csv');
+export const fetchDictionary = createAsyncThunk(
+  'words/fetchDictionary',
+  async ({initialWordId, dictionaryURL = 'dictionaries/shestov.csv'}: FetchDictionaryArgs, {dispatch, rejectWithValue}) => {
+    try {
+      const dictionaryRef = ref(firebaseStorage, dictionaryURL);
 
-    const dictionaryDownloadURL = await getDownloadURL(dictionaryRef);
+      const dictionaryDownloadURL = await getDownloadURL(dictionaryRef);
 
-    const response = await axios.get(dictionaryDownloadURL);
+      const response = await axios.get(dictionaryDownloadURL);
 
-    const dictionary: Word[] = await csv({
-      ignoreEmpty: true,
-      colParser: {
-        id: 'number',
-      },
-    }).fromString(response.data);
+      const dictionary: Word[] = await csv({
+        ignoreEmpty: true,
+        colParser: {
+          id: 'number',
+        },
+      }).fromString(response.data);
 
-    dispatch(initWord(dictionary[initialWordId]));
+      dispatch(initWord(dictionary[initialWordId]));
 
-    return dictionary;
-  } catch (error) {
-    const rejectError: Error = {name: 'unknown error', message: 'An unknown error occurred. Failed to load data.'};
+      return dictionary;
+    } catch (error) {
+      const rejectError: Error = {name: 'unknown error', message: 'An unknown error occurred. Failed to load data.'};
 
-    if (error instanceof Error) {
-      rejectError.name = error.name;
-      rejectError.message = error.message;
+      if (error instanceof Error) {
+        rejectError.name = error.name;
+        rejectError.message = error.message;
+      }
+
+      return rejectWithValue(rejectError);
     }
-
-    return rejectWithValue(rejectError);
   }
-});
+);
